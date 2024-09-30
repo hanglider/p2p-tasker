80577636194f2d74ffaad71e6b4ad01fb6d5a473
hanglider <trovimovbro@gmail.com>
2024-09-30 14:21:44 +0300

    [server]

--- server.py
+++ server.py
@@ -2,11 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/home")
+def home():
+    return "глеб красава"
+
+@app.route("/dom")
+def dom():
+    return "арсений лох ибани"
+
 @app.route('/message', methods=['POST'])
 def receive_message():
     data = request.json  # Получаем данные из POST-запроса
     print(f"Получено сообщение: {data['message']}")
-    return jsonify({'response': 'Сообщение получено!'}), 200  # Возвращаем ответ клиенту
+    return jsonify({'response': 'Сообщение получено!'})  # Возвращаем ответ клиенту
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Сервер доступен по любому IP на порту 5000
+    app.run(host='172.20.10.2', port=5000)  # Нужно вставить ip роутера
